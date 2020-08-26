9d4a7127887e709f9b8f389a2268347eae4b2f69
Deeptiman - PC <pattnaikdeeptiman@gmail.com>
2020-08-27 00:52:58 +0530

    Renamed the project to 'go-cache-kubernetes', Readme updated with project description and implementation

--- docker_script.sh
+++
@@ -1,2 +0,0 @@
-docker rm $(docker ps -a -q)
-docker-compose -f docker-compose.yml up -d --build
--- install_swagger.sh
+++
@@ -1,3 +0,0 @@
-docker pull quay.io/goswagger/swagger
-alias swagger="docker run --rm -it -e GOPATH=$HOME/go:/go -v $HOME:$HOME -w $(pwd) quay.io/goswagger/swagger"
-
